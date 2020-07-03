453b37019aadf962ab6566ce28c76a90344e89de
pawel-gawron <65308689+pawel-gawron@users.noreply.github.com>
2020-07-03 14:26:42 +0200

    spaceship_new
    setorigin

--- EventHorizon/spaceship_new.cpp
+++ EventHorizon/spaceship_new.cpp
@@ -9,17 +9,29 @@
 #include "spaceship_new.hpp"
 Spaceship_new::Spaceship_new(float x, float y,  sf::Texture &texture, int velocity_x, int velocity_y)
 {
-//    alpha = 10;
-//    r = 10;
+    //    alpha = 10;
+    //    r = 10;
     
     //setPosition(x, y);
     setTexture(texture);
-//    setPosition(0,0);
-    setScale(1.2, 1.2);
-//    setOrigin(-100, 10);
-//    setOrigin(31,-435);
+
+int size = texture.getSize().y;
+    //setScale(1.2, 1.2);
+    setOrigin((texture.getSize().x)/12, size-500);
+    setPosition(500, 500);
+    //    setOrigin(31,-435);
     setTextureRect(sf::IntRect(0,0,70,72));
     
     
 
+}
+
+void Spaceship_new::animuj(const sf::Time &elapsed)
+{
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        rotate(50*elapsed.asSeconds());
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        rotate(-50*elapsed.asSeconds());
+    }
 }
--- EventHorizon/spaceship_new.hpp
+++ EventHorizon/spaceship_new.hpp
@@ -19,5 +19,7 @@ class Spaceship_new :public sf::Sprite
 {
 public:
     Spaceship_new(float x, float y,  sf::Texture &texture, int velocity_x, int velocity_y);
+    void animuj(const sf::Time &elapsed);
+
 };
 #endif /* spaceship_new_hpp */
